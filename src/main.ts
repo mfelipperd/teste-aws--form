59b97bba024a498ba7d95ead32ad81a712ea4f8e
mfelipperd <felipperabelodurans@gmail.com>
2023-04-25 19:35:10 -0300

    altera config do cors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: 'https://form-expomm.vercel.app, https://form-expomm.vercel.app', // Substitua pelo domínio do seu frontend do Next.js
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+
+  app.enableCors({
+    origin: 'https://form-expomm.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  };
-  app.enableCors(corsOptions);
-  await app.listen(3001);
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
