26d2b653fa67d514a5e3877c682fd251a93af0b9
mfelipperd <felipperabelodurans@gmail.com>
2023-04-25 19:28:40 -0300

    links do cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: 'https://form-expomm.vercel.app/', // Substitua pelo domínio do seu frontend do Next.js
+    origin: 'https://form-expomm.vercel.app, https://form-expomm.vercel.app', // Substitua pelo domínio do seu frontend do Next.js
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
