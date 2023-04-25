a9a7d956a42f8f14cbfff47c08ae6749bf9231cb
mfelipperd <felipperabelodurans@gmail.com>
2023-04-25 18:40:29 -0300

    cors option no main.js

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  const corsOptions: CorsOptions = {
+    origin: 'https://form-expomm.vercel.app/', // Substitua pelo domínio do seu frontend do Next.js
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+  await app.listen(3001);
 }
 bootstrap();
